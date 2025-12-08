a7b6de6a24b09fc9efec58d90279c02d8747a5f5
Aïssatou <aissa.ndiaye@outlook.com>
2025-12-08 11:10:50 +0100

    Linking Login with App.js

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -32,7 +32,7 @@ export default function LoginScreen() {
 
 return (
     <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-      <Image style={styles.image} source={require('../assets/logo-rond.jpg')} />
+      <Image style={styles.image} source={require('../assets/logo-rond.png')} />
 
       {/* Bouton Sign In */}
       <TextInput placeholder="Signin" onChangeText={(value) => setSignin(value)} value={signin} style={styles.input} />
